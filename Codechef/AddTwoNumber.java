ff6a75f31a45ec38943475bb85fa1de78fbae58f
Samyak Jain <44099124+Samyak2607@users.noreply.github.com>
2019-07-10 12:40:54 +0530

    Create AddTwoNumber.java

---
+++ Codechef/AddTwoNumber.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class sol{
+    public static void main(String args[]){
+        Scanner sc =new Scanner(System.in);
+        int tc=sc.nextInt();
+        for(int i=0;i<tc;i++){
+            int a=sc.nextInt();
+            int b=sc.nextInt();
+            System.out.println(a+b);
+        }
+    }
+}
