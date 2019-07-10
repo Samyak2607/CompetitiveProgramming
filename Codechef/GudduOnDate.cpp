8876677ba41a02c110f417b7c0a1d0ce3cd9a83d
Samyak Jain <44099124+Samyak2607@users.noreply.github.com>
2019-07-10 12:30:20 +0530

    Create GudduOnDate.cpp

---
+++ Codechef/GudduOnDate.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+#define inte unsigned long long int
+
+int main() {
+	inte q;
+	cin>>q;
+	while(q--){
+	    inte n;
+	    cin>>n;
+	    inte a=n;
+	    inte sum=0,result=0,rem=0;
+	    while(n!=0){
+	        sum+=n%10;
+	        n=n/10;
+	    }
+	    rem=sum%10!=0 ? (10-sum%10):0;
+	    result=a*10+rem;
+	    cout<<result<<endl;
+	}
+	return 0;
+}
